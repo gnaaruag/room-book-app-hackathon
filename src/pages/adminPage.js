452f45b3cbc69fbb17319269c20c6720a18db5b5
Gaurang R <68043860+gnaaruag@users.noreply.github.com>
2023-06-23 22:57:22 -0700

    boilerplate code

---
+++ src/pages/adminPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function AdminPage() {
+  return (
+    <div className="container">
+      <h1>Welcome, Admin!</h1>
+      <p>This is the admin page.</p>
+    </div>
+  );
+}
+
+export default AdminPage;
